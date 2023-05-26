b4db0edc7a17f79a31a8b3ac5c1722c378823ab2
Yuba32 <92969369+Yuba32@users.noreply.github.com>
2023-05-26 13:04:24 +0900

    [update] station 03

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\PracticeController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
