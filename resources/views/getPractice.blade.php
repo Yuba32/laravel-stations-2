cbf436bf48556df13692ba8e181afcaa85249e5d
Yuba32 <92969369+Yuba32@users.noreply.github.com>
2023-06-12 00:15:38 +0900

    [update] fix indent

--- resources/views/getPractice.blade.php
+++ resources/views/getPractice.blade.php
@@ -2,18 +2,18 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Practice</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Practice</title>
 </head>
 
 <body>
-  <ul>
+<ul>
     @foreach ($practices as $practice)
-      <li>タイトル: {{ $practice->title }}</li>
+        <li>タイトル: {{ $practice->title }}</li>
     @endforeach
-  </ul>
+</ul>
 
 </body>
 
